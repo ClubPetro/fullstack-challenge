576ca56c26c666d2b8652ed2bb29b42ee37ebf5a
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-01 14:26:40 -0300

    feat: adiciona componente EditCardModal --inicial

--- client/src/components/Card/EditCardModal/index.tsx
+++ client/src/components/Card/EditCardModal/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles';
+// import { serverAPI } from '../../../services/serverAPI';
+
+// import { IconDelete } from '../../../styles/Icons';
+// import { DateInput } from '../../DateInput';
+
+// import { Container, ModalForm } from './styles';
 
 export const EditCardModal: React.FC = () => {
-  return <Container />;
+  // const [isCloseModal, setIsCloseModal] = useState(false);
+
+  return (
+    <div />
+    // <Container closeModal={isCloseModal}>
+    //   <section>
+    //     <IconDelete
+    //       style={{ fontSize: 23 }}
+    //       onClick={() => setIsCloseModal(!isCloseModal)}
+    //     />
+    //   </section>
+    //   <ModalForm>
+    //     <label htmlFor="local">Local</label>
+    //     <Input />
+    //     <label htmlFor="date">Data</label>
+    //     <Button />
+    //     <DateInput />
+    //   </ModalForm>
+    // </Container>
+  );
 };
--- client/src/components/Card/EditCardModal/styles.ts
+++ client/src/components/Card/EditCardModal/styles.ts
@@ -1,3 +1,39 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+interface Modal {
+  closeModal: boolean;
+}
+
+export const Container = styled.div<Modal>`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+
+  ${props =>
+    props.closeModal
+      ? css`
+          opacity: 0;
+          visibility: hidden;
+        `
+      : css`
+          opacity: 1;
+          visibility: visible;
+        `}
+
+  section {
+    background: #f0f2f5;
+    padding: 2.4rem;
+    width: 90%;
+    max-width: 500px;
+    position: relative;
+    z-index: 1;
+  }
+`;
+
+export const ModalForm = styled.form``;
